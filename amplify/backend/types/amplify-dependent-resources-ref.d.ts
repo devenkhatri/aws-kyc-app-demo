b5b46ec482c2668fea06e3ae0ae20adfd980e49e
Deven Goratela <deven.goratela@tcs.com>
2023-06-01 17:22:20 +0530

    First commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "kycapp350059b3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "faceId": {
+            "region": "string",
+            "celebrityDetectionEnabled": "string",
+            "maxEntities": "string"
+        },
+        "textID": {
+            "region": "string",
+            "format": "string"
+        },
+        "textInterpret": {
+            "region": "string",
+            "type": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsconfig from './aws-exports';
+import KYCContainer from './components/KYCContainer';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <KYCContainer />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
